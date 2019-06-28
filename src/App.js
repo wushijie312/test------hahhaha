4aa80b71e1e3ddda19e951f1c0f97fe2f5cd90f6
wushijie312 <1281310367@qq.com>
2019-06-28 18:15:18 +0800

    ww

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Routes from './routes/index';
+import './App.css'
+import {Provider} from 'mobx-react'
+import stores from './stores/index'
+class App extends Component {
+    render() {
+        return (
+          
+            <div className="app">
+             <Provider {...stores}>
+                <Routes />
+           </Provider>
+           </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        );
+    }
 }
 
 export default App;
---
+++ src/stores/index.js
@@ -0,0 +1,7 @@
+import aboutStore from './about'
+import homeStore from './home'
+const store ={
+    aboutStore,
+    homeStore
+}
+export default store;

b3e89e89f49bc4462aba9dd8932b084e05ddd203
wushijie312 <1281310367@qq.com>
2019-06-28 18:51:03 +0800

    Revert "ww"
    This reverts commit 4aa80b71e1e3ddda19e951f1c0f97fe2f5cd90f6.

--- src/stores/home.js
+++
@@ -1,9 +0,0 @@
-import {observable} from 'mobx';
-const HomeStore=observable({
-    title:'this is home pages',
-    // timers:new Date().getTime(),
-    getList(){
-        this.title="this is home pages"+new Date().getTime()
-    }
-});
-export default HomeStore;
